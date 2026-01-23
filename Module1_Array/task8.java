6ca9aa512f1c6aa56cc655a9900bfdcc58b45f8b
bhuvanaharshini <241801370013@cutmap.ac.in>
2026-01-23 14:30:59 +0530

    Create task8.java

---
+++ Module1_Array/task8.java
@@ -0,0 +1,31 @@
+class Node{
+   private int data;
+    Node next;
+    Node(int a){
+        data=a;
+        next=null;
+        
+    }
+    public void getdata(){
+        System.out.println(data);
+    }
+    public void getref(){
+        System.out.println(next);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Rishi");
+        Node a =new Node(10);
+        
+        Node  b=new Node(18);
+        a.getdata();
+        a.getref();
+        
+        a.next=b;
+        
+        a.getref();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
